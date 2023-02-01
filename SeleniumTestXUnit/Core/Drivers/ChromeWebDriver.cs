a70d931a4a7e0ade7abad61302befcf481a8f58a
Sch <Schlvf>
2023-02-01 14:23:41 -0500

    Implemented selenium XUnit project

---
+++ SeleniumTestXUnit/Core/Drivers/ChromeWebDriver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest.Core.Drivers;
+
+public class ChromeWebDriver
+{
+    public IWebDriver Driver { get; }
+    private readonly ConfigBuilder _config = ConfigBuilder.Instance;
+
+    public ChromeWebDriver()
+    {
+        Driver = new ChromeDriver();
+        Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(
+            _config.GetInt("DriverImplicitTimeout")
+        );
+        Driver.Manage().Window.Maximize();
+    }
+}
+
+/*
+public class ChromeWebDriver
+{
+    public IWebDriver Driver { get; }
+    private readonly ConfigBuilder _config = ConfigBuilder.Instance;
+
+    private static readonly ChromeWebDriver _instance = new ChromeWebDriver();
+
+    private ChromeWebDriver()
+    {
+        Driver = new ChromeDriver();
+        Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(
+            _config.GetInt("DriverImplicitTimeout")
+        );
+        Driver.Manage().Window.Maximize();
+    }
+
+    public void Dispose()
+    {
+        Driver.Dispose();
+    }
+
+    public static ChromeWebDriver Instance => _instance;
+}
+*/
---
+++ SeleniumTestXUnit/PageObjectModels/LoginPage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using OpenQA.Selenium;
+using SeleniumTest.Core;
+using SeleniumTest.Core.Drivers;
+
+public class LoginPage : IDisposable
+{
+    public ChromeWebDriver WebDriver;
+
+    public LoginPage()
+    {
+        WebDriver = new ChromeWebDriver();
+    }
+
+    public readonly string HostUrl = "https://todo.ly";
+    public readonly string EmailCredentials = ConfigBuilder.Instance.GetString("TODO-LY-EMAIL");
+    public readonly string PassCredentials = ConfigBuilder.Instance.GetString("TODO-LY-PASSWORD");
+
+    public readonly string LoginButtonClass = "HPHeaderLogin";
+    public readonly string EmailInputId = "ctl00_MainContent_LoginControl1_TextBoxEmail";
+    public readonly string PassInputId = "ctl00_MainContent_LoginControl1_TextBoxPassword";
+    public readonly string LoginInputId = "ctl00_MainContent_LoginControl1_ButtonLogin";
+
+    public void LoginIntoApplication()
+    {
+        WebDriver.Driver.Navigate().GoToUrl(HostUrl);
+
+        var loginButton = WebDriver.Driver.FindElement(By.ClassName(LoginButtonClass));
+        loginButton.Click();
+
+        var emailInput = WebDriver.Driver.FindElement(By.Id(EmailInputId));
+        emailInput.SendKeys(EmailCredentials);
+
+        var passInput = WebDriver.Driver.FindElement(By.Id(PassInputId));
+        passInput.SendKeys(PassCredentials);
+
+        var loginInput = WebDriver.Driver.FindElement(By.Id(LoginInputId));
+        loginInput.Click();
+    }
+
+    public void Dispose()
+    {
+        WebDriver.Driver.Dispose();
+    }
+}
